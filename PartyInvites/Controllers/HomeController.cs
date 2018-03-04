5dda2af0d6d5c27fdfe823600aef928e0b459f34
Andy Fox <16908179+fandrewfox@users.noreply.github.com>
2018-03-03 17:00:57 -0800

    Add MyView.cshtml

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello World";
+            return View("MyView");
         }
     }
 }
